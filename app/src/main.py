5b31903da963783543101b6bbf64c83fc90a6e0d
Gosia <malgorzata.belka.ue@gmail.com>
2025-11-18 01:03:22 +0100

    probes

--- app/src/main.py
+++ app/src/main.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"msg": "Hello from GKE with GitOps"}
+
+@app.get("/live")
+def liveness():
+    # Simple "am I alive" check
+    return {"status": "ok"}
+
+@app.get("/ready")
+def readiness():
+    # Here you could later check DB, external services, etc.
+    return {"status": "ready"}
